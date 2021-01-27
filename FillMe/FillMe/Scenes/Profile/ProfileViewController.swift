ea2f579caccfa30fe93c5951070bede8d508c633
Sue Cho <suejeong97@naver.com>
2021-01-27 17:48:36 +0900

    [Feat] Embed UITableVC in VC

--- FillMe/FillMe/Scenes/Profile/ProfileViewController.swift
+++ FillMe/FillMe/Scenes/Profile/ProfileViewController.swift
@@ -7,23 +7,14 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+final class ProfileViewController: UIViewController {
+    
+    //MARK:- IBOutlets
+    @IBOutlet weak var imageNameView: NameImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        imageNameView.profileNameLabel.text = "Sueaty"
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
