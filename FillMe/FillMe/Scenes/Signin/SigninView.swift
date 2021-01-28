8eeea56fb2e0275d054e3e1596cd171e55fd52ae
Sue Cho <suejeong97@naver.com>
2021-01-28 23:57:27 +0900

    [Feat] Signin View

---
+++ FillMe/FillMe/Scenes/Signin/SigninView.swift
@@ -0,0 +1,20 @@
+//
+//  SigninView.swift
+//  FillMe
+//
+//  Created by Sue Cho on 2021/01/28.
+//
+
+import UIKit
+
+class SigninView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
