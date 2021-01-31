5d341a80b872580062c6e31820320bc0a0624c8a
Sue Cho <suejeong97@naver.com>
2021-01-31 19:22:42 +0900

    [Feat] Implement a temporary logout method

---
+++ FillMe/FillMe/Scenes/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  FillMe
+//
+//  Created by Sue Cho on 2021/01/31.
+//
+
+import Foundation
+
+struct User {
+    var name: String
+    var email: String
+    var uid: String
+}
