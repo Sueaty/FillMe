6ed25cdbd92c7940a6c2c16f68f0ccb3ccf59d90
Sue Cho <suejeong97@naver.com>
2021-02-22 19:35:07 +0900

    [Feat] Add a collection view in home vc

--- FillMe/FillMe/Scenes/Home/HomeViewController.swift
+++ FillMe/FillMe/Scenes/Home/HomeViewController.swift
@@ -10,24 +10,65 @@ import Firebase
 
 class HomeViewController: UIViewController {
     
+    //MARK:- Views
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        
+        let collectionView = UICollectionView(frame: .zero,
+                                              collectionViewLayout: layout)
+        collectionView.isScrollEnabled = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+
+    // TO DO:
+        // define header & footer view
     
-    
-    @IBAction func logoutButtonTouched(_ sender: UIButton) {
-        do {
-            try Auth.auth().signOut()
-        } catch let signOutError as NSError {
-            print ("Error signing out: %@", signOutError)
-        }
-    }
-    
+    //MARK:- View Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.isHidden = true
+        
+        
+        
         if let user = Auth.auth().currentUser {
             // do something
             print("User: \(user.uid)")
         } else {
             // navigate to login
             print("User should login")
+        }
+        
+        collectionView.backgroundColor = .black
+        // add subviews
+        view.addSubview(collectionView)
+        
+        // constraints
+        var constraints = [NSLayoutConstraint]()
+        constraints.append(contentsOf: [
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor,
+                                                    constant: 10),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor,
+                                                     constant: -10),
+            collectionView.topAnchor.constraint(equalTo: view.readableContentGuide.topAnchor,
+                                                constant: 10),
+            collectionView.heightAnchor.constraint(equalTo: view.readableContentGuide.heightAnchor,
+                                                   multiplier: 0.5)
+        ])
+        
+        NSLayoutConstraint.activate(constraints)
+
+    }
+    
+    //MARK:- IBActions
+    @IBAction func logoutButtonTouched(_ sender: UIButton) {
+        do {
+            try Auth.auth().signOut()
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
         }
     }
     
